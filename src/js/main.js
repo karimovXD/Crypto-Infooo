3424b7f1c2964c4794c2caf83e516c44df3f0090
HOT-DOG <rightl2053@gmail.com>
2023-11-29 22:47:33 +0500

    style part finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,21 @@
 fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cbnb%2Cxrp%2Cusdc%2Csolana%2Ccardano%2Cdogecoin%2Cavalanche&vs_currencies=usd&include_24hr_change=true')
-.then(data => data.json())
-.then(dataJson => {
-    const container = document.querySelector(".container")
-    const coins = Object.getOwnPropertyNames(dataJson)
+    .then(data => data.json())
+    .then(dataJson => {
+        const container = document.querySelector(".container")
+        const coins = Object.getOwnPropertyNames(dataJson)
 
-    for(let menu of coins) {
-        const crypto = dataJson[menu]
-        const cryptoStatic = crypto.usd_24h_change.toFixed(7)
+        for (let menu of coins) {
+            const crypto = dataJson[menu]
+            const cryptoStatic = crypto.usd_24h_change.toFixed(7)
 
-        container.innerHTML += `
+            container.innerHTML += `
         <div class="crypto ${(cryptoStatic < 0) ? 'down' : 'up'}">
         <div class="crypto-logo">
             <img src="images/${menu}.webp">
-        </div>
         <div class="crypto-name">
             <h2>${menu}</h2>
             <p>USD</p>
+        </div>
         </div>
         <div class="crypto-price">
             <p class="crypto-usd">$${crypto.usd}</p>
@@ -23,6 +23,6 @@ fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cte
         </div>
     </div>
         `
-    }
+        }
 
-});
+    });
